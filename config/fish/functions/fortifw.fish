42e9a67fab445c5adaffdd2dcea213c407028650
Jesper Petersen <jesper.petersen@patientsky.com>
2022-06-27 08:42:31 +0200

    Helper functions

---
+++ config/fish/functions/fortifw.fish
@@ -0,0 +1,5 @@
+# Defined in - @ line 1
+function fortifw
+	echo FW Token
+	sudo openfortivpn -c /usr/local/etc/openfortivpn/openfortivpn/fw
+end
---
+++ config/fish/functions/fortimgnt.fish
@@ -0,0 +1,5 @@
+# Defined in - @ line 1
+function fortimgnt
+	echo MGNT Token
+	sudo openfortivpn -c /usr/local/etc/openfortivpn/openfortivpn/mgnt
+end
